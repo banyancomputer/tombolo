cbd6ade160228f3153034a3a22f51bc15dac5058
thea-exe <csokkunthea@gmail.com>
2023-04-21 11:56:59 -0400

    2/3 tests

--- cypress/e2e/login_page.cy.ts
+++ cypress/e2e/login_page.cy.ts
@@ -1,4 +1,7 @@
-describe('Login Page', () => {
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+describe('Logging in as a user with no demo data', () => {
     beforeEach(() => {
         cy.viewport(1280, 720)
       })
@@ -14,7 +17,26 @@ describe('Login Page', () => {
        
         cy.get('button[type="submit"]').click()
         cy.url().should('include', '/')
+
+
+        cy.get('a[href="/upload-portal"]').click();
+        cy.url().should('include', '/upload-portal');
+
+        cy.get('a[href="/account"]').click();
+        cy.url().should('include', '/account');
+
+        cy.get('a[href="mailto:support@tombolo.store"]').click();
+        cy.url().should('include', 'mailto:support@tombolo.store');
+
+        cy.get('div').contains('Log Out').click();
+        cy.url().should('include', '/login');
+
+
+        
     });
   });
+
+
+
 
   
